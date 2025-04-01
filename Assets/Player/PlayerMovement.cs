e2c8f1a31bdbbac3ef55f8c062e4891a3452c9cb
Splonge <oksanen383@gmail.com>
2025-04-01 20:26:34 +0300

    Player movement + basic ground

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float acceleration;
+    public float groundSpeed;
+    public float jumpSpeed;
+
+    public float groundDecay;
+
+    [Range(0f, 1f)]
+    public Rigidbody2D body;
+    public BoxCollider2D groundCheck;
+    public LayerMask groundMask;
+    public bool grounded;
+    float xInput;
+    float yInput;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetInput();
+        HandleJump();
+    }
+    void GetInput(){
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+    }
+    void MoveWithInput(){
+        if (Mathf.Abs(xInput) > 0){
+
+            float increment = xInput * acceleration;
+            float newSpeed = Mathf.Clamp(body.linearVelocityX + increment, -groundSpeed, groundSpeed);
+            body.linearVelocity = new Vector2(newSpeed, body.linearVelocityY);
+
+            float direction = Mathf.Sign(xInput);
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+    }
+    void HandleJump(){
+        if (Input.GetButtonDown("Jump") && grounded){
+            body.linearVelocity = new Vector2(body.linearVelocityX, jumpSpeed);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        CheckGround();
+        ApplyFriction();
+        MoveWithInput();
+    }
+     void CheckGround() {
+            grounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+        }
+    void ApplyFriction(){
+        if (grounded && xInput == 0 && body.linearVelocityY <= 0) {
+            body.linearVelocityX *= groundDecay;
+        }
+    }
+}
