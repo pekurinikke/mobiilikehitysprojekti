f3e108f1906ecfa1f1455c351b35de1eb90f8a2f
Splonge <oksanen383@gmail.com>
2025-04-04 18:42:10 +0300

    Hazard added

--- Assets/EndLevelTrigger.cs
+++ Assets/EndLevelTrigger.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndLevelTrigger : MonoBehaviour
 {
-    public string sceneName;
-
-    void OnTriggerEnter2D(Collider2D collision)
+        void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player"){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
