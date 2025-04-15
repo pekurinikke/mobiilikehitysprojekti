6f51ae6603b3c82d98bb0d53af69b41089a336d4
Splonge <oksanen383@gmail.com>
2025-04-15 20:55:02 +0300

    Popupwindow

---
+++ Assets/Popup/PopUpEngage.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using System.Collections.Generic;
+public class PopUpEngage : MonoBehaviour
+{
+    public TMP_Text popupText;
+
+    private GameObject window;
+    private Animator popupAnimator;
+    private Queue<string> popupQueue;
+    private bool isActive;
+    private Coroutine queueChecker;
+
+    private void Start()
+    {
+        window = transform.GetChild(0).gameObject;
+        popupAnimator = window.GetComponent<Animator>();
+        window.SetActive(false);
+        popupQueue = new Queue<string>();
+    }
+
+    public void AddToQueue(string text){
+        popupQueue.Enqueue(text);
+        if(queueChecker==null)
+            queueChecker = StartCoroutine(CheckQueue());
+    }
+
+    private void ShowPopup(string text){
+        isActive = true;
+        window.SetActive(true);
+        popupText.text = text;
+        popupAnimator.Play("Popup");
+    }
+
+    private IEnumerator CheckQueue(){
+        do {
+            ShowPopup(popupQueue.Dequeue());
+            do {
+                yield return null;
+            } while (!popupAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Idle"));
+        } while (popupQueue.Count > 0);
+        isActive = false;
+        window.SetActive(false);
+        queueChecker = null;
+    }
+}
